527f715555e740ea3e044748e1c13a88540fc77f
Hamatane Diallo <dhamatane@gmail.com>
2023-01-21 16:43:26 +0000

    add commet

--- php/enonce1.php
+++ php/enonce1.php
@@ -1,5 +1,13 @@
 <?php
+// Ecrire un script qui affiche les nombres de 0 a 10 en utilisant une boucle "for"
 
 for($i = 1; $i <= 10; $i++) {
     echo $i . "\n";
 }
+
+/*
+    La boucle "for" est definie avec une varialble $i initialise a 1, avec une condition
+    $i <= 10 qui est verifiee a chaque tour de boucle et une incrementation de $i a chaque
+    tour de boucle. La boucle s'execute donc 10 fois, de 1 a 10, et chaque tour, on affiche
+    la valeur de $i a l'aide "echo" suivi d'un saut de ligne.
+*/
