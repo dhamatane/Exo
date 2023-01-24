9970f62e487ce646b287ccf40ee05ce948af96bd
Hamatane Diallo <dhamatane@gmail.com>
2023-01-24 11:20:42 +0000

    add comments

--- enonce2.php
+++ enonce2.php
@@ -1,12 +1,21 @@
 <?php
-    echo "Entrez un nombre entier : ";
-    $nombre = trim(fgets(STDIN));
+    /*
+        Ecrire un script qui demande a l'utilisateur de saisir un nombre entier
+        et qui affiche la table de multiplication ce nombre en utilisant la boucle "for"
+        echo "Entrez un nombre entier : ";
+    */
 
+    // On demande a l'utilisateur de saisir un nombre entier
+    echo "Veuillez entrer un nombre entier: ";
+    $nombre = trim(fgets(STDIN));
+    
+    // On verifie si le nombre saisi est bien un entier
     if(!filter_var($nombre, FILTER_VALIDATE_INT)) {
-        echo "Vieullez rentrer un nombre entier";
+        echo "Entrez un nombre entier valide";
         exit;
     }
 
+    // Affiche la table de multiplicateur
     for($i = 1; $i <= 10; $i++) {
-        echo "$nombre x $i = " . $nombre * $i . "\n"; 
+        echo "$nombre x $i = " . $nombre * $i . "\n";
     }
